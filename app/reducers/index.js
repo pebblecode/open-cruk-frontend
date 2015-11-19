ab25ab1f12c7309e2e713e1f5c6b593affd02518
Jonathan King <jonathan@pebblecode.com>
2015-11-19 13:48:33 +0000

    Added first sidebar statements

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,6 +3,31 @@ import {
 }
 from '../actions';
 
+const SIMILARITY_THRESHOLD = 1;
+const ENGLISH_AVERAGES = {
+  'oneYearSurvivalRate': 68.2,
+  'specialist': 95.3,
+  'firstTreatment': 85.8
+};
+
+function findComparison(sample, average) {
+  if (Math.abs(sample - average) < SIMILARITY_THRESHOLD) {
+    return 'similar to';
+  } else if (sample < average) {
+    return 'worse than';
+  } else {
+    return 'better than';
+  }
+}
+
+function calcAverages(ccg) {
+  return {
+    'oneYearSurvivalRate': findComparison(ccg.oneYearSurvivalRate, ENGLISH_AVERAGES.oneYearSurvivalRate),
+    'specialist': findComparison(ccg.specialist, ENGLISH_AVERAGES.specialist),
+    'firstTreatment': findComparison(ccg.firstTreatment, ENGLISH_AVERAGES.firstTreatment)
+  };
+}
+
 export default function usersReducer(state = {
   isFetching: false,
   points: [],
@@ -47,6 +72,8 @@ export default function usersReducer(state = {
       const point = state.points.filter(p => p.ccg === state.ccgCodeSelected);
 
       point.info = action.ccg;
+      point.info.averages = calcAverages(action.ccg);
+      point.info.englishAverages = ENGLISH_AVERAGES;
 
       return Object.assign({}, state, {
         isFetching: false,
