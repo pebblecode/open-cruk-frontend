1cb243349713695834fce4516a07d60a89a8adac
Jonathan King <jonathan@pebblecode.com>
2015-11-19 14:30:22 +0000

    added top figures and titles to sidebar

--- app/components/HexMap/InfoPanel/index.jsx
+++ app/components/HexMap/InfoPanel/index.jsx
@@ -11,12 +11,28 @@ class InfoPanel extends Component {
       infoContents = (
         <div>
           <h2>{this.props.ccgSelected.name}</h2>
+          <div className='topFiguresPanel'>
+            <div className='topFigureBox'>
+              <div className='topFigure'>{this.props.ccgSelected.incidences}</div>
+              <span className='topFigureLabel'>cancer cases per year</span>
+            </div>
+            <div className='topFigureBox'>
+              <div className='topFigure'>{this.props.ccgSelected.deaths}</div>
+              <span className='topFigureLabel'>cancer deaths per year</span>
+            </div>
+          </div>
+          <h3>One-year survival rate</h3>
           <p>One-year cancer survival in {this.props.ccgSelected.name}, 
-          ({this.props.ccgSelected.oneYearSurvivalRate}%) is {this.props.ccgSelected.averages.oneYearSurvivalRate} the English average ({this.props.ccgSelected.englishAverages.oneYearSurvivalRate}%).</p>
+          ({this.props.ccgSelected.oneYearSurvivalRate}%) is {this.props.ccgSelected.averages.oneYearSurvivalRate} the English average ({this.props.ccgSelected.englishAverages.oneYearSurvivalRate}%).
+          </p>
+          <h3>Speed of referral</h3>
           <p>The proportion of patients in {this.props.ccgSelected.name} that are urgently referred with suspected cancer and see a specialist within two weeks ({this.props.ccgSelected.specialist}%) 
-           is {this.props.ccgSelected.averages.specialist} the English average ({this.props.ccgSelected.englishAverages.specialist}%).</p>
+           is {this.props.ccgSelected.averages.specialist} the English average ({this.props.ccgSelected.englishAverages.specialist}%).
+          </p>
+          <h3>Speed of treatment</h3>
           <p>The proportion of patients in {this.props.ccgSelected.name} that receive their first treatment for cancer within 62 days of an urgent GP referral ({this.props.ccgSelected.firstTreatment}%) 
-          is {this.props.ccgSelected.averages.firstTreatment} the English average ({this.props.ccgSelected.englishAverages.firstTreatment}%).</p>
+          is {this.props.ccgSelected.averages.firstTreatment} the English average ({this.props.ccgSelected.englishAverages.firstTreatment}%).
+          </p>
         </div>
       );
     }
