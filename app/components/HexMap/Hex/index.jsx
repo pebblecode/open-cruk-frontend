c9f456a6af40f98c5f2f69917470f5009afa8b0a
Emma Nash <emma.nash@pebblecode.com>
2016-03-22 14:07:16 +0000

    Add highlighted ccg tooltip

--- app/components/HexMap/Hex/index.jsx
+++ app/components/HexMap/Hex/index.jsx
@@ -7,26 +7,29 @@ class Hex extends Component {
     const {
       className,
       isEven,
-      leftStyle,
-      middleStyle,
-      rightStyle
+      colour
     } = this.props;
     const hexClass =
       isEven
       ? 'hex even ' + className
       : 'hex ' + className;
+    const leftStyle = colour ? { 'borderRightColor': colour } : {};
+    const middleStyle = colour ? { 'background': colour } : {};
+    const rightStyle = colour ? { 'borderLeftColor': colour } : {};
     return (
       <div {...this.props} className={hexClass}>
         <div className='left' style={leftStyle} />
         <div className='middle' style={middleStyle} />
         <div className='right' style={rightStyle} />
+        {this.props.children}
       </div>
     );
   }
 }
 
 Hex.propTypes = {
-  isEven: PropTypes.bool
+  isEven: PropTypes.bool,
+  colour: PropTypes.string
 };
 
 export default Hex;
