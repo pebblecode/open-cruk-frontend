804d195babed10b42f12d27a1c915553df01524d
Emma Nash <emma.nash@pebblecode.com>
2016-04-19 16:09:40 +0100

    Add header links

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -6,12 +6,12 @@ class Header extends Component {
   render() {
     return (
       <header className="gbl-head">
-        <a href="/" className="gbl-head-logo">pebblecode</a>
+        <a href="http://pebblecode.com" className="gbl-head-logo">pebblecode</a>
 
 
         <nav className="gbl-head-nav">
           <a href="/" className="gbl-head-nav-item" >Data API using CRUK data</a>
-          <a href="/" className="gbl-head-nav-item btn">Tell me more</a>
+          <a href="http://pebblecode.com/blog/cruk-data-hack" className="gbl-head-nav-item btn">Tell me more</a>
         </nav>
       </header>
 
